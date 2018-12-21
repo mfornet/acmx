27d202f5b28f43f08e9a8fea89ebdbe5c78fd5af
Marcelo Fornet <mfornet94@gmail.com>
2018-12-20 23:00:09 -0500

    Implement add problem
    Declare other functions

--- src/extension.ts
+++ src/extension.ts
@@ -2,26 +2,87 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { exec } from 'child_process';
+
+// Create a new problem
+async function add_problem() {
+    if (vscode.workspace.workspaceFolders === undefined) {
+        vscode.window.showErrorMessage("Open the folder that will contain the problem.");
+        return;
+    }
+
+    let path = vscode.workspace.workspaceFolders[0].uri.path;
+
+    let site_info = await vscode.window.showQuickPick(
+        [
+            { label: 'Codeforces', target: 'codeforces' },
+            // TODO: Disable this for real application
+            { label: 'Mock', description: 'Fake site for experimentation', target: 'mock' },
+        ],
+        { placeHolder: 'Select contest site' });
+
+    if (site_info === undefined){
+        vscode.window.showErrorMessage("Site not provided.");
+        return;
+    }
+
+    let site = site_info.target;
+
+    let pid = await vscode.window.showInputBox({placeHolder: "Problem ID"});
+
+    if (pid === undefined){
+        vscode.window.showErrorMessage("Problem ID not provided.");
+        return;
+    }
+
+    // TODO: Use builtin path join from typescript to be cross-platform
+    path += `/${pid}`;
+
+    let command = `acmh problem ${site} ${pid} -p ${path}`;
+
+    exec(command, function(error, stdout, stderr) {
+        console.log(command);
+        console.log(stdout);
+        console.log(stderr);
+
+        vscode.window.showInformationMessage(`Add problem ${site}/${pid} at ${path}`);
+    });
+
+    // TODO: Open new problem folder with active window at sol.cpp
+}
+
+async function add_contest() {
+    vscode.window.showInformationMessage("Add Contest coming SOON!");
+}
+
+async function run_solution(){
+    vscode.window.showInformationMessage("Run solution coming SOON!");
+}
+
+async function stress(){
+    vscode.window.showInformationMessage("Stress coming SOON!");
+}
+
+async function upgrade(){
+    vscode.window.showInformationMessage("Upgrade coming SOON!");
+}
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "acmhelper-vscode" is now active!');
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
+    let add_problem_commmand = vscode.commands.registerCommand('extension.addProblem', add_problem);
+    let add_contest_command = vscode.commands.registerCommand('extension.addContest', add_contest);
+    let run_solution_command = vscode.commands.registerCommand('extension.runSolution', run_solution);
+    let stress_command = vscode.commands.registerCommand('extension.stress', stress);
+    let upgrade_command = vscode.commands.registerCommand('extension.upgrade', upgrade);
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-    });
-
-    context.subscriptions.push(disposable);
+    context.subscriptions.push(add_problem_commmand);
+    context.subscriptions.push(add_contest_command);
+    context.subscriptions.push(run_solution_command);
+    context.subscriptions.push(stress_command);
+    context.subscriptions.push(upgrade_command);
 }
 
 // this method is called when your extension is deactivated
