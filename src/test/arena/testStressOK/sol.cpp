08b3040e0f42cbb9bd93ceda37af5f64c4a26219
Marcelo Fornet <mfornet94@gmail.com>
2018-12-22 17:49:17 -0500

    Remove test contest/problems data from repository

--- src/test/arena/testStressOK/sol.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-using namespace std;
-
-int main(){
-   int n; cin >> n;
-   cout << n + 2 << endl;
-   return 0;
-}
--- src/test/arena/testStressWA/sol.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-using namespace std;
-
-int main(){
-   int n; cin >> n;
-   cout << n + 3 << endl;
-   return 0;
-}
