d918dcdc0a58c8ed111f95961f8110359adc0474
Marcelo Fornet <mfornet94@gmail.com>
2020-03-07 19:08:10 -0500

    test: Use rimraf

--- src/test/test_utils.ts
+++ src/test/test_utils.ts
@@ -1,37 +1,13 @@
 
-import { closeSync, existsSync, lstatSync, openSync, readdirSync, rmdirSync, unlinkSync, writeSync } from 'fs';
-import { join } from 'path';
+import { closeSync, existsSync, openSync, writeSync } from 'fs';
+const rimraf = require('rimraf');
 
 /**
  * Recursive remove
  */
 export function recursiveRemoveDirectory(path: string) {
     if (existsSync(path)) {
-        readdirSync(path).forEach(name => {
-            let cPath = join(path, name);
-            let cPathStat = lstatSync(cPath);
-
-            if (cPathStat.isFile()) {
-                unlinkSync(cPath);
-            } else if (cPathStat.isDirectory()) {
-                recursiveRemoveDirectory(cPath);
-            } else {
-                throw Error(`Error removing ${cPath} | stat: ${cPathStat}`);
-            }
-        });
-
-        let retries = 3;
-        while (retries > 0) {
-            try {
-                rmdirSync(path);
-                break;
-            } catch (error) {
-                retries -= 1;
-                if (retries === 0) {
-                    throw (path);
-                }
-            }
-        }
+        rimraf.sync(path);
     }
 }
 
