42960a805e04b96108e6594fab77c7e7430b4121
Marcelo Fornet <mfornet94@gmail.com>
2020-05-09 17:31:52 -0400

    Use arrow functions

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -18,9 +18,7 @@ const CONTEST = join(pathToStatic(), "testData", "exampleContest");
 suite("Extension Test Suite", () => {
     vscode.window.showInformationMessage("Start all tests.");
 
-    test("New Arena", function () {
-        this.timeout(5000);
-
+    test("New Arena", () => {
         runWithTemporaryPath((path: string) => {
             newArena(path);
             assert.ok(existsSync(join(path, "sol.cpp")));
@@ -29,49 +27,43 @@ suite("Extension Test Suite", () => {
         });
     });
 
-    // test("Upgrade Arena", function () {
-    //     this.timeout(5000);
+    test("Upgrade Arena", () => {
+        runWithTemporaryPath((path: string) => {
+            newArena(path);
+            upgradeArena(path);
 
-    //     runWithTemporaryPath((path: string) => {
-    //         newArena(path);
-    //         upgradeArena(path);
+            assert.ok(existsSync(join(path, "gen.py")));
+            assert.ok(existsSync(join(path, "brute.cpp")));
+        });
+    });
 
-    //         assert.ok(existsSync(join(path, "gen.py")));
-    //         assert.ok(existsSync(join(path, "brute.cpp")));
-    //     });
-    // });
+    test("Parse Test Cases", () => {
+        let path = join(CONTEST, "A");
+        let result = testCasesName(path);
+        let target = ["0", "1", "2"];
 
-    // test("Parse Test Cases", function () {
-    //     this.timeout(5000);
+        assert.equal(result.length, target.length);
+        result.sort();
 
-    //     let path = join(CONTEST, "A");
-    //     let result = testCasesName(path);
-    //     let target = ["0", "1", "2"];
+        for (let i = 0; i < 3; ++i) {
+            assert.equal(target[i], result[i]);
+        }
+    });
 
-    //     assert.equal(result.length, target.length);
-    //     result.sort();
-
-    //     for (let i = 0; i < 3; ++i) {
-    //         assert.equal(target[i], result[i]);
-    //     }
-    // });
-
-    // test("Create new problem from id", function () {
-    //     this.timeout(5000);
-
-    //     runWithTemporaryPath((path: string) => {
-    //         let problemId = "mockProblem";
-    //         newProblemFromId(path, MOCK_SITE, problemId);
-    //         let problemPath = join(path, problemId);
-    //         assert.ok(existsSync(problemPath));
-    //         assert.ok(existsSync(join(problemPath, "sol.cpp")));
-    //         assert.ok(existsSync(join(problemPath, ATTIC)));
-    //         assert.ok(existsSync(join(problemPath, TESTCASES)));
-    //         assert.equal(
-    //             readdirSync(join(problemPath, TESTCASES)).length,
-    //             6,
-    //             "Incorrect number of files."
-    //         );
-    //     });
-    // });
+    test("Create new problem from id", () => {
+        runWithTemporaryPath((path: string) => {
+            let problemId = "mockProblem";
+            newProblemFromId(path, MOCK_SITE, problemId);
+            let problemPath = join(path, problemId);
+            assert.ok(existsSync(problemPath));
+            assert.ok(existsSync(join(problemPath, "sol.cpp")));
+            assert.ok(existsSync(join(problemPath, ATTIC)));
+            assert.ok(existsSync(join(problemPath, TESTCASES)));
+            assert.equal(
+                readdirSync(join(problemPath, TESTCASES)).length,
+                6,
+                "Incorrect number of files."
+            );
+        });
+    });
 });
